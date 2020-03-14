da488e344b033ff4a4132d6a9f2c174a67df9291
Julian Benedikt Heuschen <julian@fheuschen.de>
2020-03-14 16:18:48 +0100

    Initial Commit

---
+++ gc-prototype-1-te/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gc-prototype-1-te
+//
+//  Created by Julian Benedikt Heuschen on 14.03.20.
+//  Copyright © 2020 Julian Benedikt Heuschen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
